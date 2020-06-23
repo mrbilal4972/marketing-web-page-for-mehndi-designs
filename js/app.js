bc7b6cf1b1f380bae4ad0506ff4c3f15ba3407bf
mrbilal4972 <mrbilal4972@users.noreply.github.com>
2020-06-24 00:57:53 +0500

    Images Modal functionality added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,39 @@ const hand = document.getElementById('hand');
 const foot = document.getElementById('foot');
 const fullHand = document.getElementById('fullHand');
 const imgCards = document.getElementById('img-cards');
+
+
+function afterCards(){
+    const cardImg = document.getElementsByClassName('cardImg');
+        Array.from(cardImg).forEach((i) => {
+            // console.log(imgSrc)
+            i.addEventListener('click', modalHandler);
+            // i.addEventListener('blur', modalRemoveHandler);
+        })
+}
+
+function modalHandler(e){
+    const imgSrc = e.target.getAttribute('src');
+    console.log(e.target, imgSrc)
+    const modal = document.getElementById('Modal');
+    const modalBody = document.getElementById('modal-body');
+    const closeBtn = document.getElementById('closeBtn');
+        modalBody.innerHTML = '';
+        modalImg = `<img src="${imgSrc}" class="img-fluid card-img-top" style="height: auto; width: 100%;" alt="...">`;
+        modalBody.insertAdjacentHTML('beforeend', modalImg);
+        modal.classList.remove('hide');
+        modal.classList.add('show');
+        closeBtn.addEventListener('click', modalRemoveHandler);
+}
+
+function modalRemoveHandler(e){
+
+    const modal = document.getElementById('Modal');
+    // console.log(e.target)
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+
+}
 // const header = document.getElementById('header');
 
 // console.log(header);
@@ -10,7 +43,14 @@ window.addEventListener('load', addHandCard);
 hand.addEventListener('click', addHandCard);
 foot.addEventListener('click', addFootCard);
 fullHand.addEventListener('click', addFullHandCard);
+// cardImg.addEventListener('click', modalHandler)
 
+// function modalHandler(e){
+//     console.log(e.target)
+// }
+
+
+// Hand Tab
 function addHandCard(e){
     // console.log(e.target.id)
     // if(e.target.id ==='hand'){
@@ -23,16 +63,19 @@ function addHandCard(e){
     let count = 48;
     for(let i = 0; i <= count; i++){
         imgCards.insertAdjacentHTML('beforeend', `<div class="col-lg-4">
-                                                    <div class="card mt-4" style="width: 18rem;">
-                                                        <img src="./img/h${i}.jpg" class="img-fluid card-img-top" style="height: 16rem;" alt="...">
+                                                    <div class="card mt-4" style="width: 20rem;">
+                                                        <img src="./img/h${i}.jpg" class="img-fluid card-img-top cardImg" style="height: 20rem;" alt="...">
                                                         <div class="card-body">
                                                             <h5 class="card-title text-center">Design ${i+1}</h5>
                                                         </div>
                                                     </div>
                                                 </div>`);
     }
+    afterCards()
 }
 
+
+// foot Tab
 function addFootCard(e){
     e.target.classList.add('active');
     hand.classList.remove('active');
@@ -52,6 +95,8 @@ function addFootCard(e){
     }
 }
 
+
+// Full Hand Tab
 function addFullHandCard(e){
     e.target.classList.add('active');
     hand.classList.remove('active');
